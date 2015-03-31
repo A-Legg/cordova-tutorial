44cc0b6e715708bdc946e845bf7d85314df2b42a
Adam Legg <adam.legg@gmail.com>
2015-03-31 16:11:00 -0600

    added handlebars templates

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,19 +1,24 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
+  function renderHomeView() {
+    $('body').html(homeTpl());
+    $('.search-key').on('keyup', findByName);
+}
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
+        renderHomeView();
         console.log("Service initialized");
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+
 
     document.addEventListener('deviceready', function () {
+      FastClick.attach(document.body);
         if (navigator.notification) { // Override default HTML alert with native dialog
              window.alert = function (message) {
                     navigator.notification.alert(
@@ -28,15 +33,9 @@
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
+    service.findByName($('.search-key').val()).done(function (employees) {
+        $('.content').html(employeeListTpl(employees));
+    });
+}
 
 }());
